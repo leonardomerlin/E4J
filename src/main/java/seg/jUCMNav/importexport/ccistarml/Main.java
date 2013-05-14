7e4d6defa5a6ea0ea4148c0df1fba03f149aa811
Leonardo Merlin <contato@leonardomerlin.com.br>
2013-05-14 14:02:33 -0300

    Commit inicial: - Projeto Maven Java Default - Package CCISTARML Default

---
+++ src/main/java/seg/jUCMNav/importexport/ccistarml/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Main.java
+ * Sample of using ccistarml Java Package
+ *
+ * it is part of the ccistarml Java Package
+ * version 0.6
+ * Created on July 4 of 2007, By Carlos Cares
+ * Updated to v0.6.1  on September 20 of 2007 By Carlos Cares
+ */
+package seg.jUCMNav.importexport.ccistarml;
+import java.lang.String;
+import java.util.*;
+import java.io.*;
+import seg.jUCMNav.importexport.ccistarml.*;
+
+public class Main {
+    
+    public Main() {
+    }
+    
+    public static void main(String[] args) {
+ERelementList erL;
+ERelement erle;
+ccistarmlFile f = new ccistarmlFile ();
+f.loadFile("sample.istarml"); // put your file name here !
+f.xmlParser();
+f.istarmlParser();
+System.out.println("Cantidad de errores: "+f.errors());
+f.displayErrors();
+    }
+}
+    
+
